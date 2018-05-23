5dee684f912d6150d357a29fd665d72dbb79e2fc
LanFladimir <LanFladimir@outlook.com>
2018-05-23 18:38:53 +0800

    relayout book store

--- app/src/main/java/owner/flaimir/booksmeller/activity/book/BookCenterActivity.java
+++ app/src/main/java/owner/flaimir/booksmeller/activity/book/BookCenterActivity.java
@@ -1,29 +1,34 @@
 package owner.flaimir.booksmeller.activity.book;
 
 import android.content.Context;
-import android.databinding.DataBindingComponent;
-import android.databinding.DataBindingUtil;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import owner.flaimir.booksmeller.R;
-import owner.flaimir.booksmeller.databinding.ActivityMainBinding;
 
 /**
  * MainActivity
  */
 public class BookCenterActivity extends AppCompatActivity {
     Context mContext;
-    ActivityMainBinding mMainBinding;
+    ImageView main_bg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_bookcenter);
         mContext = this;
+        main_bg = findViewById(R.id.main_bg);
+        Glide.with(mContext)
+                .load(getImg(3))
+                .into(main_bg);
+    }
+
+    private int getImg(int id) {
+        return mContext.getResources().getIdentifier("bg_store_0" + id, "drawable",
+                mContext.getPackageName());
     }
 }
--- app/src/main/java/owner/flaimir/booksmeller/activity/splash/SplashActivity.java
+++ app/src/main/java/owner/flaimir/booksmeller/activity/splash/SplashActivity.java
@@ -1,25 +1,30 @@
 package owner.flaimir.booksmeller.activity.splash;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 import owner.flaimir.booksmeller.R;
 import owner.flaimir.booksmeller.activity.book.BookCenterActivity;
+import owner.flaimir.booksmeller.databinding.ActivitySplashBinding;
 
 /**
  * Splash
  */
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
+        ActivitySplashBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        Glide.with(this)
+                .load(R.drawable.bg_store_03)
+                .into(binding.splashImg);
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
